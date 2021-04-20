20337de029055390d29543a53ab2a18140f6ae62
mash716 <ymanshou@gmail.com>
2021-04-20 13:57:56 +0900

    seeder　追加処理 ・禁煙IDのデータを挿入する ・お店データを挿入する ・中間テーブルにデータを挿入する ・上記中間テーブルのカテゴリータイプにデータを挿入する

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ShopPay;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //禁煙IDを取得する
+        $this->call(SmokingSeeder::Class);
+
+        //お店データを挿入する
+        $this->call(ShopSeeder::class);
+
+        //中間テーブル
+        $this->call(CategorySeeder::class);
+        $this->call(PaySeeder::class);
+        $this->call(MenuTypeSeeder::class);
+        $this->call(ImageTypeSeeder::class);
+
+        //カテゴリー
+        $this->call(ShopCategorySeeder::class);
+        $this->call(ShopPaySeeder::class);
+        $this->call(MenuSeeder::class);
+        $this->call(ImageSeeder::class);
+
     }
 }
