c46ec3a9a2f6c64460e6b6e34e5bde56cace442b
mash716 <ymanshou@gmail.com>
2021-04-29 07:00:33 +0900

    予算テーブルのマイグレーションファイルを作成する

---
+++ database/migrations/2021_04_26_014100_create_budget_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBudgetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budget', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('shop_id')->unique()->nullable();
+            $table->integer('min');
+            $table->integer('max');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budget');
+    }
+}
