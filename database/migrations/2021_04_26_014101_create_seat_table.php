c3aeb6c8addca8dbeec0dbbb5e82f04b4e333013
mash716 <ymanshou@gmail.com>
2021-04-29 07:01:50 +0900

    座席テーブルのマイグレーションファイルの作成

---
+++ database/migrations/2021_04_26_014101_create_seat_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seat', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('shop_id');
+            $table->integer('type_id');
+            $table->integer('seat');
+            $table->integer('table_number');
+            $table->integer('table_seat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seat');
+    }
+}
