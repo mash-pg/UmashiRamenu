933dffef31077c926c0ad0924a3b9b194ff09b02
mash716 <ymanshou@gmail.com>
2021-04-29 06:55:23 +0900

    店舗テーブルのマイグレーションファイル修正 電話番号をvarchar(15)に設定する。

--- database/migrations/2021_04_26_014007_create_shops_table.php
+++ database/migrations/2021_04_26_014007_create_shops_table.php
@@ -16,9 +16,9 @@ class CreateShopsTable extends Migration
         Schema::create('shops', function (Blueprint $table) {
             $table->increments('id');
             $table->string('shop_name')->unique();
+            $table->string('tel', 15);
             $table->text('business_day');
             $table->text('kodawari');
-            $table->text('tel');
             $table->integer('smoking_id');
             $table->timestamps();
         });
