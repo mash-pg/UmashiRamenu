6a3738c2881c8d5e22cf1ca732c1a5a8a5fe4222
mash716 <ymanshou@gmail.com>
2021-04-29 13:49:07 +0900

    SeatTypeFactoryの作成

---
+++ database/factories/SeatTypeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\SeatType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SeatTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = SeatType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
