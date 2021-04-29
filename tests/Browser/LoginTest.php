6a41bcc59fba9104f530e235ea8f464c1b222a47
mash716 <ymanshou@gmail.com>
2021-04-29 21:37:50 +0900

    laravelduckを修正する 手順１：docker ps して idをメモってdocker exec -it [id] bash 上記にてdockerに入る 手順２：apt-get update でapt-getをアップデートする 手順３：apt-get install chromium-browser　で　インストールする 手順４；php artisan dusk:install 手順５：php artisan dusk

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -16,7 +16,7 @@ class LoginTest extends DuskTestCase
     public function testExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
+            $browser->visit('http://localhost')
                     ->assertSee('Laravel');
         });
     }
