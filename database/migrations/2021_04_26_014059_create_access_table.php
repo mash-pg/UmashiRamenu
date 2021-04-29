7971c034c7571e5babce4e981af66b2deeddb8b5
mash716 <ymanshou@gmail.com>
2021-04-29 13:19:45 +0900

    アクセステーブルのマイグレーションファイルの作成

---
+++ database/migrations/2021_04_26_014059_create_access_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('access', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('shop_id')->unique();
+            $table->integer('post_number_1');
+            $table->integer('post_number_2');
+            $table->string('prefectures');
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('address3');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('access');
+    }
+}
