797d13e729b2ead524b88caf84e46721689dd8b4
mash716 <ymanshou@gmail.com>
2021-05-01 15:00:59 +0900

    web.phpのルートの編集 ＊必要ないuseやルートの削除

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ShopController;
-use Inertia\Inertia;
+use App\Http\Controllers\Shop\ShopController;
 
 Route::resource('shops', ShopController::class);
-
-
-//
-//Route::get('/', function () {
-//    return Inertia::render('Welcome', [
-//        'canLogin' => Route::has('login'),
-//        'canRegister' => Route::has('register'),
-//        'laravelVersion' => Application::VERSION,
-//        'phpVersion' => PHP_VERSION,
-//    ]);
-//});
-//
-//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-//    return Inertia::render('Dashboard');
-//})->name('dashboard');
